0c5129d548140679446bcd8c4168a7fe169b2b69
Richard Liu <xuegang.liu@nxp.com>
2018-08-01 11:26:41 +0800

    MA-12251 Fix EvsApp resource leak issue
    EvsApp will stop after run several seconds, the root cause
    is due to resource leak.
    
    Error log:
    GraphicBufferMapper: importBuffer(0xf4b35f4578c0) failed: 5
    v_egl : _TranslateFormat: unknown android format=0
    EvsApp : error creating EGLImage for target buffer: EGL_BAD_PARAMETER
    EvsApp : Failed to attached render target
    
    Change-Id: Ic3913afdf1bbe7db8ad6a55e7517bfae5149c148
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>

--- evs/app/RenderTopView.cpp
+++ evs/app/RenderTopView.cpp
@@ -174,6 +174,11 @@ void RenderTopView::deactivate() {
 
 
 bool RenderTopView::drawFrame(const BufferDesc& tgtBuffer) {
+     // release resourse to avoid leak
+     if (tgtBuffer.memHandle.getNativeHandle() != nullptr) {
+         detachRenderTarget();
+     }
+
     // Tell GL to render to the given buffer
     if (!attachRenderTarget(tgtBuffer)) {
         ALOGE("Failed to attached render target");
