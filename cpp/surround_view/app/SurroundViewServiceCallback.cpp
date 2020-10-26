bea21a8f4c494c20f701dbd4b8f3562b89acb7b0
zhang sanshan <pete.zhang@nxp.com>
2020-10-26 14:56:11 +0800

    EVS camera can't work correctly
    detachRenderTarget should not be called at end of each frame.
    Because GPU render in async, Video buffer should be released in next turn.
    it should ben relase before attachRenderTarget
    
    Signed-off-by: zhang sanshan <pete.zhang@nxp.com>
    Change-Id: Iad659dc15ad074def68d78a3d3f401fc1f324256

--- cpp/surround_view/app/SurroundViewServiceCallback.cpp
+++ cpp/surround_view/app/SurroundViewServiceCallback.cpp
@@ -435,6 +435,11 @@ Return<void> SurroundViewServiceCallback::receiveFrames(
             tgtBuffer = buff;
         });
 
+        // release resourse to avoid leak
+        if (tgtBuffer.memHandle.getNativeHandle() != nullptr) {
+           detachRenderTarget();
+        }
+
         if (!attachRenderTarget(convertBufferDesc(tgtBuffer))) {
             LOG(ERROR) << "Failed to attach render target";
             return {};
@@ -536,10 +541,6 @@ Return<void> SurroundViewServiceCallback::receiveFrames(
 
         glDisableVertexAttribArray(0);
         glDisableVertexAttribArray(1);
-
-        // Now that everything is submitted, release our hold on the
-        // texture resource
-        detachRenderTarget();
 
         // Wait for the rendering to finish
         glFinish();
