CodeLinaro-mirror/la_platform_packages_services_Car

Add test activity to handle full screen transition to kitchensink
- the new activity shows the usage of WindowInsetsController api's to
toggle to full screen
- also shows the fast tap ux transition to/from full screen activity
bug: 186437495
Test: tested manually using emulator

Change-Id: I2a8b852aa3d4585a05db1fe8fbf16aba68b3faba

tests/EmbeddedKitchenSinkApp/src/com/google/android/car/kitchensink/KitchenSinkActivity.java
@@ -58,6 +58,7 @@ import com.google.android.car.kitchensink.displayinfo.DisplayInfoFragment;
 import com.google.android.car.kitchensink.experimental.ExperimentalFeatureTestFragment;
 import com.google.android.car.kitchensink.hvac.HvacTestFragment;
 import com.google.android.car.kitchensink.insets.WindowInsetsFragment;
+import com.google.android.car.kitchensink.insets.WindowInsetsFullScreenFragment;
 import com.google.android.car.kitchensink.notification.NotificationFragment;
 import com.google.android.car.kitchensink.orientation.OrientationTestFragment;
 import com.google.android.car.kitchensink.packageinfo.PackageInfoFragment;
@@ -204,7 +205,9 @@ public class KitchenSinkActivity extends FragmentActivity {
             new FragmentMenuEntry("vehicle hal", VehicleHalFragment.class),
             new FragmentMenuEntry("volume test", VolumeTestFragment.class),
             new FragmentMenuEntry("web links", WebLinksTestFragment.class),
-            new FragmentMenuEntry("window insets", WindowInsetsFragment.class));
+            new FragmentMenuEntry("window insets", WindowInsetsFragment.class),
+            new FragmentMenuEntry("window insets full screen",
+                    WindowInsetsFullScreenFragment.class));
 
     private Car mCarApi;
     private CarHvacManager mHvacManager;

nxp-imx-android/aosp_platform_packages_services_Car

(cherry picked from commit c1fb756a6e03fe838530930c6eecd33330b7884f)

tests/EmbeddedKitchenSinkApp/src/com/google/android/car/kitchensink/KitchenSinkActivity.java
@@ -59,6 +59,7 @@ import com.google.android.car.kitchensink.diagnostic.DiagnosticTestFragment;
 import com.google.android.car.kitchensink.displayinfo.DisplayInfoFragment;
 import com.google.android.car.kitchensink.experimental.ExperimentalFeatureTestFragment;
 import com.google.android.car.kitchensink.hvac.HvacTestFragment;
+import com.google.android.car.kitchensink.insets.WindowInsetsFullScreenFragment;
 import com.google.android.car.kitchensink.notification.NotificationFragment;
 import com.google.android.car.kitchensink.orientation.OrientationTestFragment;
 import com.google.android.car.kitchensink.packageinfo.PackageInfoFragment;
@@ -211,7 +212,9 @@ public class KitchenSinkActivity extends FragmentActivity {
             new FragmentMenuEntry("vehicle hal", VehicleHalFragment.class),
             new FragmentMenuEntry("volume test", VolumeTestFragment.class),
             new FragmentMenuEntry("watchdog", CarWatchdogTestFragment.class),
-            new FragmentMenuEntry("web links", WebLinksTestFragment.class));
+            new FragmentMenuEntry("web links", WebLinksTestFragment.class),
+            new FragmentMenuEntry("window insets full screen",
+                    WindowInsetsFullScreenFragment.class));
 
     private Car mCarApi;
     private CarHvacManager mHvacManager;