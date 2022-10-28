LineageOS/android_packages_services_Car

Add TIRAMISU_2 constant in CarVersion and PlatformVersion
Test: atest CarVersionTest
Bug: 253469571
Change-Id: Ifbd2ca63f39e117c916c22ae09bc857e78c8265b

car-lib/src/android/car/annotation/ApiRequirements.java
@@ -58,6 +58,7 @@ public @interface ApiRequirements {
 
         TIRAMISU_0(android.car.CarVersion.VERSION_CODES.TIRAMISU_0),
         TIRAMISU_1(android.car.CarVersion.VERSION_CODES.TIRAMISU_1),
+        TIRAMISU_2(android.car.CarVersion.VERSION_CODES.TIRAMISU_2),
         UPSIDE_DOWN_CAKE_0(android.car.CarVersion.VERSION_CODES.UPSIDE_DOWN_CAKE_0);
 
         private final android.car.CarVersion mVersion;
@@ -79,6 +80,7 @@ public @interface ApiRequirements {
 
         TIRAMISU_0(android.car.PlatformVersion.VERSION_CODES.TIRAMISU_0),
         TIRAMISU_1(android.car.PlatformVersion.VERSION_CODES.TIRAMISU_1),
+        TIRAMISU_2(android.car.PlatformVersion.VERSION_CODES.TIRAMISU_2),
         UPSIDE_DOWN_CAKE_0(android.car.PlatformVersion.VERSION_CODES.UPSIDE_DOWN_CAKE_0);
 
         private final android.car.PlatformVersion mVersion;

nxp-imx-android/aosp_platform_packages_services_Car

Merged-In: Ifbd2ca63f39e117c916c22ae09bc857e78c8265b
(cherry picked from commit 1ba3709eb65a44142cd8524e7c616b4b5275ae66)

car-lib/src/android/car/annotation/ApiRequirements.java
@@ -57,7 +57,8 @@ public @interface ApiRequirements {
     enum CarVersion {
 
         TIRAMISU_0(android.car.CarVersion.VERSION_CODES.TIRAMISU_0),
-        TIRAMISU_1(android.car.CarVersion.VERSION_CODES.TIRAMISU_1);
+        TIRAMISU_1(android.car.CarVersion.VERSION_CODES.TIRAMISU_1),
+        TIRAMISU_2(android.car.CarVersion.VERSION_CODES.TIRAMISU_2);
 
         private final android.car.CarVersion mVersion;
 
@@ -77,7 +78,8 @@ public @interface ApiRequirements {
     enum PlatformVersion {
 
         TIRAMISU_0(android.car.PlatformVersion.VERSION_CODES.TIRAMISU_0),
-        TIRAMISU_1(android.car.PlatformVersion.VERSION_CODES.TIRAMISU_1);
+        TIRAMISU_1(android.car.PlatformVersion.VERSION_CODES.TIRAMISU_1),
+        TIRAMISU_2(android.car.PlatformVersion.VERSION_CODES.TIRAMISU_2);
 
         private final android.car.PlatformVersion mVersion;
 