LineageOS/android_packages_services_Car

Exposed OEM service name as test API
Bug: 226406223
Test: adb shell dumpsys car_service --oem-service --name-only
Change-Id: I79025008707e83893e61b8490d2f165810219441

car-lib/src/android/car/test/CarTestManager.java
@@ -130,4 +130,17 @@ public final class CarTestManager extends CarManagerBase {
     public boolean hasAidlVhal() throws RemoteException {
         return mService.hasAidlVhal();
     }
+
+    /**
+     * Returns OEM service name.
+     *
+     * @hide
+     */
+    @TestApi
+    @RequiresPermission(Car.PERMISSION_CAR_TEST_SERVICE)
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.UPSIDE_DOWN_CAKE_0,
+             minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    public String getOemServiceName() throws RemoteException {
+        return mService.getOemServiceName();
+    }
 }

nxp-imx-android/aosp_platform_packages_services_Car

Merged-In: I79025008707e83893e61b8490d2f165810219441
(cherry picked from commit 16390f793d0bb45bd0ce7a90f91b28abea478134)

car-lib/src/android/car/test/CarTestManager.java
@@ -21,6 +21,7 @@ import android.annotation.TestApi;
 import android.car.Car;
 import android.car.CarManagerBase;
 import android.car.annotation.AddedInOrBefore;
+import android.car.annotation.ApiRequirements;
 import android.os.IBinder;
 import android.os.RemoteException;
 
@@ -89,5 +90,18 @@ public final class CarTestManager extends CarManagerBase {
         } catch (RemoteException e) {
             handleRemoteExceptionFromCarService(e);
         }
+    }
+
+    /**
+     * Returns OEM service name.
+     *
+     * @hide
+     */
+    @TestApi
+    @RequiresPermission(Car.PERMISSION_CAR_TEST_SERVICE)
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.TIRAMISU_2,
+             minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    public String getOemServiceName() throws RemoteException {
+        return mService.getOemServiceName();
     }
 }