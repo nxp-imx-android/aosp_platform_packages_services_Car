LineageOS/android_packages_services_Car

Added classes for car OEM focus evaluation
The classes include:
- OemCarAudioFocusEvaluationRequest, which contains the request
  information
- OemCarAudioFocusResult, which contains the results of the request
- AudioFocusEntry, which contains the audio focus informatin along with
  the current status of the focus request for this entry
- CarVolumeGroupId, contains the volume group id and name
Also added the API in OEM car audio focus service to evaluate the audio
focus request. In later changes this will be used in car audio service
to send the focus request to OEMs.

CTS-Coverage-Bug: 245982224
Bug: 240615622
Test: m -j
Change-Id: I3b2b604e56668d1e1397b4c7f49ba9201d5aa694

car-lib/src/android/car/oem/OemCarAudioFocusService.java
@@ -41,4 +41,31 @@ public interface OemCarAudioFocusService extends OemCarServiceComponent {
             minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
     void audioFocusChanged(@NonNull List<AudioFocusInfo> currentFocusHolders,
             @NonNull List<AudioFocusInfo> currentFocusLosers, int zoneId);
+
+    /**
+     * Call to evaluate a focus request, the request contains the information to make a decision.
+     *
+     * @param request current request containing the focus entry that triggered the current focus
+     * evaluation, the current focus holders, and current focus losers (focus requests that have
+     * transiently lost focus but can gain it again).
+     *
+     * @return the result of the focus request
+     * The result can be granted, delayed, or failed. In the case of granted the car audio stack
+     * will be changed according to the entries returned in newly loss and newly blocked.
+     * For delayed results the entry will be added as the current delayed request and it will be
+     * re-evaluated when any of the current focus holders abandons focus. For failed request,
+     * the car audio focus stack will not change and the current request will not gain focus.
+     *
+     * <p>Note: For the new focus losers and new blocked focus entries the focus loss can be
+     * permanent or transient. In the case of permanent loss the entry will receive permanent
+     * focus loss and it will be removed from the car audio focus stack. For transient losses,
+     * the new current request will become a blocker but will only receive transient focus loss.
+     * Everytime there is focus change the blocked entries will be re-evaluated to determine
+     * which can regain, lose, or continue with block focus.
+     **/
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.UPSIDE_DOWN_CAKE_0,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    @NonNull
+    OemCarAudioFocusResult evaluateAudioFocusRequest(
+            @NonNull OemCarAudioFocusEvaluationRequest request);
 }

car-lib/src/android/car/oem/OemCarAudioFocusServiceImpl.java
@@ -49,6 +49,15 @@ final class OemCarAudioFocusServiceImpl extends IOemCarAudioFocusService.Stub
     }
 
     @Override
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.UPSIDE_DOWN_CAKE_0,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    @NonNull
+    public OemCarAudioFocusResult evaluateAudioFocusRequest(
+            @NonNull OemCarAudioFocusEvaluationRequest request) {
+        return mOemCarAudioFocusService.evaluateAudioFocusRequest(request);
+    }
+
+    @Override
     public void init() {
         mOemCarAudioFocusService.init();
     }

nxp-imx-android/aosp_platform_packages_services_Car

Bug: 265211184
Merged-In: I3b2b604e56668d1e1397b4c7f49ba9201d5aa694
(cherry picked from commit 656fa15291e91e42ba419cef58d337b8408c5fd5)

car-lib/src/android/car/oem/OemCarAudioFocusService.java
@@ -39,4 +39,31 @@ public interface OemCarAudioFocusService extends OemCarServiceComponent {
             minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
     void audioFocusChanged(@NonNull List<AudioFocusInfo> currentFocusHolders,
             @NonNull List<AudioFocusInfo> currentFocusLosers, int zoneId);
+
+    /**
+     * Call to evaluate a focus request, the request contains the information to make a decision.
+     *
+     * @param request current request containing the focus entry that triggered the current focus
+     * evaluation, the current focus holders, and current focus losers (focus requests that have
+     * transiently lost focus but can gain it again).
+     *
+     * @return the result of the focus request
+     * The result can be granted, delayed, or failed. In the case of granted the car audio stack
+     * will be changed according to the entries returned in newly loss and newly blocked.
+     * For delayed results the entry will be added as the current delayed request and it will be
+     * re-evaluated when any of the current focus holders abandons focus. For failed request,
+     * the car audio focus stack will not change and the current request will not gain focus.
+     *
+     * <p>Note: For the new focus losers and new blocked focus entries the focus loss can be
+     * permanent or transient. In the case of permanent loss the entry will receive permanent
+     * focus loss and it will be removed from the car audio focus stack. For transient losses,
+     * the new current request will become a blocker but will only receive transient focus loss.
+     * Everytime there is focus change the blocked entries will be re-evaluated to determine
+     * which can regain, lose, or continue with block focus.
+     **/
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.TIRAMISU_2,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    @NonNull
+    OemCarAudioFocusResult evaluateAudioFocusRequest(
+            @NonNull OemCarAudioFocusEvaluationRequest request);
 }

car-lib/src/android/car/oem/OemCarAudioFocusServiceImpl.java
@@ -49,6 +49,15 @@ final class OemCarAudioFocusServiceImpl extends IOemCarAudioFocusService.Stub
     }
 
     @Override
+    @ApiRequirements(minCarVersion = ApiRequirements.CarVersion.TIRAMISU_2,
+            minPlatformVersion = ApiRequirements.PlatformVersion.TIRAMISU_0)
+    @NonNull
+    public OemCarAudioFocusResult evaluateAudioFocusRequest(
+            @NonNull OemCarAudioFocusEvaluationRequest request) {
+        return mOemCarAudioFocusService.evaluateAudioFocusRequest(request);
+    }
+
+    @Override
     public void init() {
         mOemCarAudioFocusService.init();
     }