LineageOS/android_packages_services_Car

Finished pending todos from car service
Added logic to get volume group for audio attribute. Also removed
todos that should have been closed previoulsy.

Bug: 265211184
Bug: 240615622
Test: atest com.android.car.audio
Change-Id: I8fdf44ac6474841994c4a6af66d1cc9b819f4424

service/src/com/android/car/audio/CarAudioService.java
@@ -2853,6 +2853,14 @@ public final class CarAudioService extends ICarAudio.Stub implements CarServiceB
         return activeAudioAttributes;
     }
 
+    int getVolumeGroupIdForAudioAttribute(int audioZoneId, AudioAttributes attributes) {
+        Objects.requireNonNull(attributes, "Audio attributes can not be null");
+        checkAudioZoneId(audioZoneId);
+        synchronized (mImplLock) {
+            return getVolumeGroupIdForAudioAttributeLocked(audioZoneId, attributes);
+        }
+    }
+
     static final class SystemClockWrapper {
         public long uptimeMillis() {
             return SystemClock.uptimeMillis();

tests/carservice_unit_test/src/com/android/car/audio/CarAudioServiceUnitTest.java
@@ -1905,6 +1905,40 @@ public final class CarAudioServiceUnitTest extends AbstractExtendedMockitoTestCa
     }
 
     @Test
+    public void getVolumeGroupIdForAudioAttribute() {
+        mCarAudioService.init();
+
+        expectWithMessage("Volume group ID for primary audio zone")
+                .that(mCarAudioService.getVolumeGroupIdForAudioAttribute(PRIMARY_AUDIO_ZONE,
+                        CarAudioContext.getAudioAttributeFromUsage(USAGE_MEDIA)))
+                .isEqualTo(TEST_PRIMARY_GROUP_INDEX);
+    }
+
+    @Test
+    public void getVolumeGroupIdForAudioAttribute_withNullAttribute_fails() {
+        mCarAudioService.init();
+
+        NullPointerException thrown = assertThrows(NullPointerException.class, () ->
+                mCarAudioService.getVolumeGroupIdForAudioAttribute(PRIMARY_AUDIO_ZONE,
+                /* attribute= */ null));
+
+        expectWithMessage("Null audio attribute exception").that(thrown).hasMessageThat()
+                .contains("Audio attributes");
+    }
+
+    @Test
+    public void getVolumeGroupIdForAudioAttribute_withInvalidZoneId_fails() {
+        mCarAudioService.init();
+
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () ->
+                mCarAudioService.getVolumeGroupIdForAudioAttribute(INVALID_AUDIO_ZONE,
+                        CarAudioContext.getAudioAttributeFromUsage(USAGE_MEDIA)));
+
+        expectWithMessage("Invalid audio zone exception").that(thrown).hasMessageThat()
+                .contains("Invalid audio zone Id");
+    }
+
+    @Test
     public void getInputDevicesForZoneId_primaryZone() {
         mCarAudioService.init();
 

tests/carservice_unit_test/src/com/android/car/audio/CarVolumeInfoWrapperTest.java
@@ -83,6 +83,8 @@ public final class CarVolumeInfoWrapperTest {
                 .thenReturn(CALL);
         when(mMockCarAudioService.getVolumeGroupIdForAudioContext(PRIMARY_AUDIO_ZONE, CALL))
                 .thenReturn(TEST_GROUP_ID);
+        when(mMockCarAudioService.getVolumeGroupIdForAudioAttribute(PRIMARY_AUDIO_ZONE,
+                TEST_MEDIA_AUDIO_ATTRIBUTE)).thenReturn(TEST_GROUP_ID);
         when(mMockCarAudioService.getGroupVolume(PRIMARY_AUDIO_ZONE, TEST_GROUP_ID))
                 .thenReturn(TEST_GROUP_VOLUME);
         when(mMockCarAudioService.getGroupMinVolume(PRIMARY_AUDIO_ZONE, TEST_GROUP_ID))
@@ -128,6 +130,15 @@ public final class CarVolumeInfoWrapperTest {
                 .getVolumeGroupIdForAudioZone(PRIMARY_AUDIO_ZONE);
 
         assertWithMessage("Car Audio Group Id")
+                .that(groupId).isEqualTo(TEST_GROUP_ID);
+    }
+
+    @Test
+    public void getVolumeGroupIdForAudioAttribute_returnsGroupId() {
+        int groupId = mCarVolumeInfoWrapper
+                .getVolumeGroupIdForAudioAttribute(PRIMARY_AUDIO_ZONE, TEST_MEDIA_AUDIO_ATTRIBUTE);
+
+        assertWithMessage("Car Audio Group Id for Media Audio Attribute")
                 .that(groupId).isEqualTo(TEST_GROUP_ID);
     }
 

nxp-imx-android/aosp_platform_packages_services_Car

Merged-In: I8fdf44ac6474841994c4a6af66d1cc9b819f4424
(cherry picked from commit 1ddfc33544abdea9fe2628a8c147edc1f76e5251)

service/src/com/android/car/audio/CarAudioService.java
@@ -1683,6 +1683,14 @@ public class CarAudioService extends ICarAudio.Stub implements CarServiceBase {
         return activeAudioAttributes;
     }
 
+    int getVolumeGroupIdForAudioAttribute(int audioZoneId, AudioAttributes attributes) {
+        Objects.requireNonNull(attributes, "Audio attributes can not be null");
+        synchronized (mImplLock) {
+            checkAudioZoneIdLocked(audioZoneId);
+            return getVolumeGroupIdForAudioAttributeLocked(audioZoneId, attributes);
+        }
+    }
+
     static final class SystemClockWrapper {
         public long uptimeMillis() {
             return SystemClock.uptimeMillis();

tests/carservice_unit_test/src/com/android/car/audio/CarAudioServiceUnitTest.java
@@ -1147,6 +1147,40 @@ public final class CarAudioServiceUnitTest extends AbstractExtendedMockitoTestCa
     }
 
     @Test
+    public void getVolumeGroupIdForAudioAttribute() {
+        mCarAudioService.init();
+
+        assertWithMessage("Volume group ID for primary audio zone")
+                .that(mCarAudioService.getVolumeGroupIdForAudioAttribute(PRIMARY_AUDIO_ZONE,
+                        CarAudioContext.getAudioAttributeFromUsage(USAGE_MEDIA)))
+                .isEqualTo(TEST_PRIMARY_GROUP_INDEX);
+    }
+
+    @Test
+    public void getVolumeGroupIdForAudioAttribute_withNullAttribute_fails() {
+        mCarAudioService.init();
+
+        NullPointerException thrown = assertThrows(NullPointerException.class, () ->
+                mCarAudioService.getVolumeGroupIdForAudioAttribute(PRIMARY_AUDIO_ZONE,
+                /* attribute= */ null));
+
+        assertWithMessage("Null audio attribute exception").that(thrown).hasMessageThat()
+                .contains("Audio attributes");
+    }
+
+    @Test
+    public void getVolumeGroupIdForAudioAttribute_withInvalidZoneId_fails() {
+        mCarAudioService.init();
+
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () ->
+                mCarAudioService.getVolumeGroupIdForAudioAttribute(INVALID_AUDIO_ZONE,
+                        CarAudioContext.getAudioAttributeFromUsage(USAGE_MEDIA)));
+
+        assertWithMessage("Invalid audio zone exception").that(thrown).hasMessageThat()
+                .contains("Invalid audio zone Id");
+    }
+
+    @Test
     public void getExternalSources_forSingleDevice() {
         mCarAudioService.init();
         AudioDeviceInfo[] inputDevices = generateInputDeviceInfos();

tests/carservice_unit_test/src/com/android/car/audio/CarVolumeInfoWrapperTest.java
@@ -73,6 +73,8 @@ public final class CarVolumeInfoWrapperTest {
         when(carAudioService.getSuggestedAudioContextForPrimaryZone()).thenReturn(CALL);
         when(carAudioService.getVolumeGroupIdForAudioContext(PRIMARY_AUDIO_ZONE, CALL))
                 .thenReturn(TEST_GROUP_ID);
+        when(carAudioService.getVolumeGroupIdForAudioAttribute(PRIMARY_AUDIO_ZONE,
+                TEST_MEDIA_AUDIO_ATTRIBUTE)).thenReturn(TEST_GROUP_ID);
         when(carAudioService.getGroupVolume(PRIMARY_AUDIO_ZONE, TEST_GROUP_ID))
                 .thenReturn(TEST_GROUP_VOLUME);
         when(carAudioService.getGroupMinVolume(PRIMARY_AUDIO_ZONE, TEST_GROUP_ID))
@@ -118,6 +120,15 @@ public final class CarVolumeInfoWrapperTest {
                 .getVolumeGroupIdForAudioZone(PRIMARY_AUDIO_ZONE);
 
         assertWithMessage("Car Audio Group Id")
+                .that(groupId).isEqualTo(TEST_GROUP_ID);
+    }
+
+    @Test
+    public void getVolumeGroupIdForAudioAttribute_returnsGroupId() {
+        int groupId = mCarVolumeInfoWrapper
+                .getVolumeGroupIdForAudioAttribute(PRIMARY_AUDIO_ZONE, TEST_MEDIA_AUDIO_ATTRIBUTE);
+
+        assertWithMessage("Car Audio Group Id for Media Audio Attribute")
                 .that(groupId).isEqualTo(TEST_GROUP_ID);
     }
 